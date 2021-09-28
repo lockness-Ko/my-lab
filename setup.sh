9cc4d8f7372a5f5a8f95577b30fdd122a3404e1a
Lockness <42625905+lockness-Ko@users.noreply.github.com>
2021-09-28 11:40:22 +1000

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -2,22 +2,22 @@
 sudo apt install tmux; sudo apt install zsh; sudo apt install exa; sudo apt install nmap; sudo apt install tree; sudo apt install cmatrix; sudo apt install ncat; sudo apt install sshpass; sudo apt install openvpn
 
 # Setup tmux
-cp ./.tmux.conf ~/.tmux.conf
-cp ./.tmux_status.sh ~/.tmux_status.sh
+mv ./.tmux.conf ~/.tmux.conf
+mv ./.tmux_status.sh ~/.tmux_status.sh
 chmod +x ~/.tmux_status.sh
-cp ./.tmux_right.sh ~/.tmux_right.sh
+mv ./.tmux_right.sh ~/.tmux_right.sh
 chmod +x ~/.tmux_right.sh
-cp ./.tmux_wlan.sh ~/.tmux_wlan.sh
+mv ./.tmux_wlan.sh ~/.tmux_wlan.sh
 chmod +x ~/.tmux_wlan.sh
-cp ./.tmux_openvpn.sh ~/.tmux_openvpn.sh
+mv ./.tmux_openvpn.sh ~/.tmux_openvpn.sh
 chmod +x ~/.tmux_openvpn.sh
-cp ./.zshrc ~/.zshrc
+mv ./.zshrc ~/.zshrc
 
 mkdir ~/tools
 
-cp ./tools/block_ssh.sh ~/tools/block_ssh.sh
-cp ./tools/storage.sh ~/tools/storage.sh
-cp ./tools/nmaper.sh ~/tools/nmaper.sh
+mv ./tools/block_ssh.sh ~/tools/block_ssh.sh
+mv ./tools/storage.sh ~/tools/storage.sh
+mv ./tools/nmaper.sh ~/tools/nmaper.sh
 
 sudo usermod --shell /usr/bin/zsh `whoami`
 
